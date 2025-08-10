d539284f7a0bf61d37ec0ebb48b7f181b25e2f78
Aisha Abdullahi <ana9@calvin.edu>
2025-08-10 16:05:01 -0400

    r9

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ const createElement = (html, className) => {
 }
 
 const getChatResponse = async(incomingChatDiv) => {
-    const API_URL = "https://chatgpt-clone-trrq.onrender.com"; // Update with your server URL
+    const API_URL = "https://chatgpt-clone-trrq.onrender.com/api/chat"; // Update with your server URL
     const pElement = document.createElement("p");
 
 
